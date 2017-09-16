2872d737511fb36ba4474154133802476c03fe4d
Alex Tickle <alexander.tickle@gmail.com>
2017-09-16 10:37:04 -0700

    all rest endpoints working correctly

--- server/app.js
+++ server/app.js
@@ -11,29 +11,33 @@ app.use(bodyParser.json());
 app.post('/login', (request, response) =>
   User.findOne({
     where: { email: request.body.email }
-  }).then(user => {
-    if (user && user.password === request.body.password) {
-      response.status(200);
-      response.json({
-        message: 'Success!',
-        user: user
-      });
-    } else {
-      response.status(400);
-      response.json({ message: 'invalid email and/or password' });
-    }
   })
+    .then(user => {
+      if (user && user.password === request.body.password) {
+        response.status(200);
+        response.json({
+          user: user
+        });
+      } else {
+        response.status(400);
+        response.json({ message: 'invalid email and/or password' });
+      }
+    })
+    .catch(error => {
+      response.status(400);
+      response.json({ errors: error.errors });
+    })
 );
 
 app.post('/create-message', (request, response) =>
   Message.create(request.body)
     .then(message => {
       response.status(200);
-      response.json({ message: 'success', message: message });
+      response.json({ message: message });
     })
     .catch(error => {
       response.status(400);
-      response.json({ message: 'error', errors: error.errors });
+      response.json({ errors: error.errors });
     })
 );
 
@@ -45,11 +49,11 @@ app.get('/messages/:id', (request, response) =>
   })
     .then(messages => {
       response.status(200);
-      response.json({ message: 'success', messages: messages });
+      response.json({ messages: messages });
     })
     .catch(error => {
       response.status(400);
-      response.json({ message: 'error', errors: error.errors });
+      response.json({ errors: error.errors });
     })
 );
 
